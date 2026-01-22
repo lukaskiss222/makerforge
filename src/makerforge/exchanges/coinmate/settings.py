e83b3d72cf97a3522fba99b7b5cd8cd21c110861
lukaskiss222 <lukaskiss222@gmail.com>
2026-01-22 12:59:23 +0100

    fix subcommand env

--- src/makerforge/exchanges/coinmate/settings.py
+++ src/makerforge/exchanges/coinmate/settings.py
@@ -3,4 +3,7 @@ from pydantic import BaseModel, Field
 
 class CoinmateSettings(BaseModel):
     websocket: str = Field("wss://coinmate.io/api/websocket/channel", description="Websocket url for coinmate.")
-    pair: str = Field(description="Coinmate pair like BTC_EUR.")
+    pair: str = Field(
+        ...,
+        description="Coinmate pair like BTC_EUR.",
+    )
--- src/makerforge/settings.py
+++ src/makerforge/settings.py
@@ -5,7 +5,9 @@ from .exchanges.coinmate.settings import CoinmateSettings
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", cli_parse_args=True)
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", cli_parse_args=True, env_nested_delimiter="__"
+    )
     redis_dsn: RedisDsn = Field(
         RedisDsn("redis://localhost:6379/1"),
         validation_alias=AliasChoices("service_redis_dsn", "redis_url", "redis-url"),
