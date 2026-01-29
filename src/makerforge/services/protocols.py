51b9eb0b5f515b6557067e12c59d809970fc42d9
lukaskiss222 <lukaskiss222@gmail.com>
2026-01-29 16:37:35 +0100

    remove covaraint

--- src/makerforge/services/protocols.py
+++ src/makerforge/services/protocols.py
@@ -3,8 +3,8 @@ from typing import Protocol, TypeVar
 
 import pydantic as py
 
-T = TypeVar("T", bound=py.BaseModel, covariant=True)
+T = TypeVar("T", bound=py.BaseModel)
 
 
-class ExchangeConnectorProtocol(Protocol[T]):
+class ExchangeConnectorProtocol(Protocol):
     def __call__(self, pair: str) -> AsyncGenerator[T]: ...
